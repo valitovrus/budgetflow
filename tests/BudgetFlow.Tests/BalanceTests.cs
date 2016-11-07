1326ea49a6f5383638082d629b7fae5d0510a67e
valitovrus <valitov.rus@gmail.com>
2016-11-07 19:37:27 +0300

    added unit test skeleton project

---
+++ tests/BudgetFlow.Tests/BalanceTests.cs
@@ -0,0 +1,20 @@
+﻿using BudgetFlow.Models;
+using System;
+using Xunit;
+
+namespace BudgetFlow.Tests
+{
+    public class BalanceTests
+    {
+        [Fact]
+        public void CopyToCopiesAllFields() 
+        {
+            var balance = new Balance() { Amount = 10, Date = DateTime.Now };
+            var targetBalance = new Balance();
+            balance.CopyTo(targetBalance);
+
+            Assert.Equal(balance.Amount, targetBalance.Amount);
+            Assert.Equal(balance.Date, targetBalance.Date);
+        }
+    }
+}
